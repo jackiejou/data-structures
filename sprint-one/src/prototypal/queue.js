35593c173f7a432a37774d1bbc66ca085064bd1f
Ryan Ngo <rayango92@gmail.com>
2017-11-29 12:09:01 -0800

    finished stack with prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.position = 0;
+  queue.storage = {};
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+  	this.position++;
+  	this.storage[this.position] = value;
+  },
+  dequeue: function() {
+  	var item = this.storage[Object.keys(this.storage)[0]];
+  	delete this.storage[Object.keys(this.storage)[0]];
+  	return item;
+  },
+  size: function() {
+  	return Object.keys(this.storage).length;
+  }
+};
 
 
