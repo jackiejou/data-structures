70271e00a415ab5fb7520a02b5cbd14a48b9b810
jackiejou <joujackie@gmail.com>
2017-11-29 19:54:56 -0800

    Complete Part 1 Data Structure

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  // saving the value into storage with key index
+  // increment index
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() {
+  // get first key value pair from storage
+  // save those key value pairs as variables
+  // delete key value pairs from storage
+  // return key value pair
+  var firstKey = Object.keys(this.storage)[0];
+  var firstValue = this.storage[firstKey];
+  delete this.storage[firstKey];
+  return firstValue;
+};
+
+Queue.prototype.size = function() {
+  // return length of object keys of storage
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
 
+Stack.prototype.push = function(value) {
+  // save the value inside storage with index key
+  // increment index
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+Stack.prototype.pop = function() {
+  // get the last keyvalue pairs from storage
+  // save them as two variables
+  // delete the last keyvalue pair
+  // decrement index
+  // return value 
+  var lastKey = Object.keys(this.storage)[this.index - 1];
+  var lastValue = this.storage[lastKey];
+  delete this.storage[lastKey];
+  this.index--;
+  return lastValue;
+};
+
+Stack.prototype.size = function() {
+  // return length of storage's object keys
+  return Object.keys(this.storage).length;
+};
