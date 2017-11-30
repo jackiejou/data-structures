3b66113093ff7b142668770a49cfcba8553d8bd9
jackiejou <joujackie@gmail.com>
2017-11-30 13:58:35 -0800

    resubmit linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,34 +1,59 @@
-var Stack = function() {
-  var instance = Object.create(stackMethods);
-  instance.storage = {};
-  instance.index = 0;
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
 
-  return instance;
+  list.addToTail = function(value) {
+    // create a new node and assign value to the paramenter
+    // if head is not referencing any value, then reference the new node, and reference  tail to the same node
+    // else, have the existing tail node reference new node and then reassign tail to the new node
+    var newNode = new Node (value);
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+  };
+
+  list.removeHead = function() {
+    // create variable to hold head node value
+    // reassign head to head.next node
+    // return variable
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
+  };
+
+  list.contains = function(target) {
+    // create a current node variable and assign to head
+    // do while loop with condition as currentNode does not equal null
+      // check if currentNode value is equal to target
+      // else reassign currentNode to next
+    // return false if not found
+    var currentNode = list.head;
+    
+    do {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    } while (currentNode);
+    return false;
+  };
+  return list;
 };
 
-var stackMethods = {};
+var Node = function(value) {
+  var node = {};
 
-stackMethods.push = function(value) {
-  // save value at storage at index key
-  // increment index
-  this.storage[this.index] = value;
-  this.index++;
+  node.value = value;
+  node.next = null;
+
+  return node;
 };
 
-stackMethods.pop = function() {
-  // get last key value from storage and save as two variables
-  // delete last key value pair from storage
-  // decrement index
-  // return lastValue
-  var lastKey = Object.keys(this.storage)[this.index - 1];
-  var lastValue = this.storage[lastKey];
-  delete this.storage[lastKey];
-  this.index--;
-  return lastValue;
-};
-
-stackMethods.size = function() {
-  // return length of the keys in storage
-  return Object.keys(this.storage).length;
-};
-
+/*
+ * Complexity: What is the time complexity of the above functions? O(n)
+ */
