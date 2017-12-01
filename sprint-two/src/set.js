06078e96394c1d309cc11ec49dbc0d52e7849ac3
jackiejou <joujackie@gmail.com>
2017-11-30 20:01:43 -0800

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // check if item exists in storage
+    // if not, push item in storage
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  // check if item is inside storage
+  return this.storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  // get index of item in storage and remove it
+  this.storage.splice(this.storage.indexOf(item), 1);
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? O(n)
  */
